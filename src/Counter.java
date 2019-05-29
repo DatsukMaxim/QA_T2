48afc39a2ccd597a6652180a165036179f07feee
DatsukMaxim <48429319+DatsukMaxim@users.noreply.github.com>
2019-05-29 15:01:05 +0700

    -m

--- src/Counter.java
+++ src/Counter.java
@@ -1,20 +1,29 @@
 import java.io.*;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Counter {
-    public static void main(String[] args) throws Exception {
-
+    public static void main(String[] args) {
         try (FileReader reader = new FileReader("string.txt")){
-            int countOfWords = 0;
+            LineNumberReader lineNumberReader = new LineNumberReader(reader);
+            int countOfWords, i = 0;
+            ArrayList<String> arrayList = new ArrayList<>();
 
             Scanner scanner = new Scanner(reader);
-            String string = scanner.nextLine();
-
-            countOfWords = string.split(" ").length;
-            System.out.println(countOfWords + " words");
-
+            while (scanner.hasNextLine())
+            {
+                arrayList.add(scanner.nextLine());
+                countOfWords = arrayList.get(i).split(" ").length;
+                //System.out.print(arrayList.get(i));
+                if (i==0)
+                    System.out.println("\t\t" + countOfWords + " words");
+                else System.out.println();
+                i++;
+            }
+            reader.close();
             scanner.close();
-        }catch (IOException ex){
+        }
+        catch (IOException ex){
             System.out.println(ex.getMessage());
         }
     }
