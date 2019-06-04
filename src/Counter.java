57c957d87991a15345621e4e8787f9b83e58da3b
DatsukMaxim <48429319+DatsukMaxim@users.noreply.github.com>
2019-06-04 10:51:02 +0700

    -m

--- src/Counter.java
+++ src/Counter.java
@@ -16,7 +16,7 @@ public class Counter {
                 countOfWords = arrayList.get(i).split("\\s+").length;
                 //System.out.print(arrayList.get(i)+"\t\t");
                 if (i==0)
-                    System.out.println(countOfWords + " words");
+                    System.out.println(countOfWords);
                 else System.out.println();
                 i++;
             }
